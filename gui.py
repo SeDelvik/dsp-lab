7cf95636db350ae64850b08d54b145a9b30a93a8
adubrovina <adubrovina@at-consulting.ru>
2023-03-10 17:23:44 +0300

    changes

---
+++ gui.py
@@ -0,0 +1,99 @@
+from tkinter import *
+from tkinter import filedialog
+from PIL import Image, ImageTk  # Подключим необходимые библиотеки.
+
+from edge_detection import create_image
+
+
+class Gui:
+
+    def __init__(self):
+        self.variables = {}
+
+        self.window = Tk()
+        self.window.title("Добро пожаловать")
+        self.window.geometry('950x650')
+        self.methode_var = IntVar(value=0)
+
+        lbl = Label(self.window, text="Укажите scr-файл")
+        lbl.grid(column=0, row=0, columnspan=2)
+        btn = Button(self.window, text="Выбрать", command=self.get_image)
+        btn.grid(column=0, row=1)
+        self.lbl_src = Label(self.window, text="")
+        self.lbl_src.grid(column=1, row=1)
+
+        # создание двух канвас для отображения входного изображения и с выделенными контурами
+        self.image_input = None
+        self.photo_input = None
+
+        self.canvas_input = Canvas(self.window, height=400, width=400)
+        self.c_image_input = None
+        self.canvas_input.grid(row=2, column=0)
+
+        self.image_output = None
+        self.photo_output = None
+
+        self.canvas_output = Canvas(self.window, height=400, width=400)
+        self.c_image_output = None
+        self.canvas_output.grid(row=2, column=1)
+
+        self.create_radio_buttons()  # создание кнопок для выбора нужного алгоритма
+
+        btn_start = Button(self.window, text="Начать", command=self.create_edge_image)
+        btn_start.grid(column=0, row=8)
+
+        self.window.mainloop()
+
+    def get_image(self):
+        """
+        Вызов окна выбора файла, получение пути до изображения и вставка его в канвас.
+        :return:
+        """
+        file = filedialog.askopenfilename(filetypes=(("PNG", "*.png"), ("PCX", "*.pcx"), ("BMP", "*.bmp"),
+                                                     ("all files", "*.*")))
+        self.lbl_src.configure(text=f"{file}")
+        self.variables["src"] = file
+        print(self.variables["src"])
+        self.image_input = Image.open(file)
+        self.image_input = self.image_input.resize((400, 400), Image.ANTIALIAS)
+        self.photo_input = ImageTk.PhotoImage(self.image_input)
+        self.c_image_input = self.canvas_input.create_image(0, 0, anchor='nw', image=self.photo_input)
+
+    def create_edge_image(self):
+        """
+        Вызов алгоритмов выделения контуров и вставка результата в канвас.
+        :return:
+        """
+        if "src" not in self.variables:
+            return
+        src_out = create_image(self.variables["src"], self.methode_var.get())
+        self.image_output = Image.open(src_out)
+        self.image_output = self.image_output.resize((400, 400), Image.ANTIALIAS)
+        self.photo_output = ImageTk.PhotoImage(self.image_output)
+        self.c_image_output = self.canvas_output.create_image(0, 0, anchor='nw', image=self.photo_output)
+
+    def create_radio_buttons(self):
+        """
+        Создание радиобаттон для выбора нужного алгоритма.
+        :return:
+        """
+        btn1 = Radiobutton(self.window, text="Выделение линий", value=0, variable=self.methode_var)
+        btn2 = Radiobutton(self.window, text="Курсовые градиентные", value=1, variable=self.methode_var)
+        btn3 = Radiobutton(self.window, text="Лаплас", value=2, variable=self.methode_var)
+        btn4 = Radiobutton(self.window, text="Робертс", value=3, variable=self.methode_var)
+        btn5 = Radiobutton(self.window, text="Превитт", value=4, variable=self.methode_var)
+        btn6 = Radiobutton(self.window, text="Собел", value=5, variable=self.methode_var)
+        btn7 = Radiobutton(self.window, text="Кирш", value=6, variable=self.methode_var)
+
+        lbl_line_algs = Label(self.window, text="Линейные алгоритмы")
+        lbl_not_line_algs = Label(self.window, text="Не линейные алгоритмы")
+        lbl_line_algs.grid(column=0, row=3)
+        lbl_not_line_algs.grid(column=1, row=3)
+
+        btn1.grid(column=0, row=4)
+        btn2.grid(column=0, row=5)
+        btn3.grid(column=0, row=6)
+        btn4.grid(column=1, row=4)
+        btn5.grid(column=1, row=5)
+        btn6.grid(column=1, row=6)
+        btn7.grid(column=1, row=7)
--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from PIL import Image, ImageDraw  # Подключим необходимые библиотеки.
+from gui import Gui
 
 def main():
-    print('test')
+    # mode = int(input('mode:'))  # Считываем номер преобразования.
+    image = Image.open("./input/test_image.jpg")  # Открываем изображение.
+
+    width = image.size[0]  # Определяем ширину.
+    height = image.size[1]  # Определяем высоту.
+    # pix = image.load()  # Выгружаем значения пикселей.
+    gray = image.convert("L")
+    draw = ImageDraw.Draw(gray)  # Создаем инструмент для рисования.
+    gray_pix = gray.load()
+    print(gray_pix[0, 0])
+
+
+
+    # image.save("ans.jpg", "JPEG")  # сохранение результата
+    # del draw  # удаление кисти
+    Gui()
 
 
 if __name__ == '__main__':
